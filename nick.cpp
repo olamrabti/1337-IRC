78b57ff765a03a62d6c868ee573ab874c9e20527
J3Ss0u <90192954+J3Ss0u@users.noreply.github.com>
2025-01-29 17:46:32 +0100

    inform other clients about the change of nickname

--- nick.cpp
+++ nick.cpp
@@ -1,5 +1,20 @@
 #include "Server.hpp"
 
+
+void Server::broadcastNickChange(Client &client, const std::string &oldNick, const std::string &newNick)
+{
+    std::string message = ":" + oldNick + "!" + client.getUsername() + "@" + client.getHostName() + " NICK :" + newNick + "\r\n";
+
+    // Broadcast to all clients
+    for (std::map<int, Client>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+    {
+        if (it->second.getClientFd() != client.getClientFd())
+        {
+            sendReply(it->second.getClientFd(), message);
+        }
+    }
+}
+
 void Server::NickCommand(int client_fd, std::vector<std::string> command)
 {
     if (command.size() < 2)
@@ -8,10 +23,25 @@ void Server::NickCommand(int client_fd, std::vector<std::string> command)
         return;
     }
     Client &currClient = _clients[client_fd];
-    if (currClient.getAuthStatus() != 0x01 && currClient.getAuthStatus() != 0x05 && currClient.getAuthStatus() != 0x07 && currClient.getAuthStatus() != 0x03)
+    if (currClient.getAuthStatus() < 0x01)
         return;
 
     std::string nickname = command[1];
+    int i = 0;
+    bool validName = false;
+    while (nickname[i])
+    {
+        if (isalnum(nickname[i]))
+            validName = true;
+        i++;
+    }
+
+    if (!validName)
+    {
+        sendReply(client_fd, ERR_NONICKNAMEGIVEN(_clients[client_fd].getNickname(), _clients[client_fd].getHostName()));
+        return;
+    }
+
     for (std::map<int, Client>::iterator it = _clients.begin(); it != _clients.end(); ++it)
     {
         if (it->second.getNickname() == nickname)
@@ -20,12 +50,19 @@ void Server::NickCommand(int client_fd, std::vector<std::string> command)
             return;
         }
     }
+    // // std::cout << "Nickname set to " << nickname << " for client " << client_fd << std::endl;
+    std::string oldNick = currClient.getNickname();
     currClient.setNickname(nickname);
-    currClient.setAuthStatus(0x02);
-    std::cout << "Nickname set to " << nickname << " for client " << client_fd << std::endl;
-    if (currClient.getAuthStatus() == 0x07 && currClient.getNickFlag() == 0)
+
+    if (currClient.getAuthStatus() == 0x07 && currClient.getNickFlag() == 0 && oldNick.empty())
     {
         sendWelcomeMessages(client_fd, currClient);
         currClient.setNickFlag(1);
+    }
+
+    currClient.setAuthStatus(0x02);
+    if (currClient.isFullyAuthenticated())
+    {
+        broadcastNickChange(currClient, oldNick, nickname);
     }
 }
