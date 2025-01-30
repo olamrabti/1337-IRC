18bcee016d21b608aa3f462373ebe2a58a5be8c9
J3Ss0u <90192954+J3Ss0u@users.noreply.github.com>
2025-01-30 17:14:13 +0100

    setAuthStatus 0x01 only first time

--- pass.cpp
+++ pass.cpp
@@ -7,16 +7,21 @@ void Server::PassCommand(int client_fd, std::vector<std::string> command)
         sendReply(client_fd, ERR_NEEDMOREPARAMS(_clients[client_fd].getNickname(), _clients[client_fd].getHostName(), "PASS"));
         return;
     }
+
     std::string password = command[1];
     Client &currClient = _clients[client_fd];
-
-    if (currClient.getAuthStatus() == 0x07)
+    if (_password == password && currClient.getAuthStatus() == 0x07)
     {
         sendReply(client_fd, ERR_ALREADYREGISTERED(currClient.getNickname(), currClient.getHostName()));
         return;
     }
+
     if (_password == password)
-        currClient.setAuthStatus(0x01);
-    else
+    {
+        if (currClient.getAuthStatus() < 0x01)
+            currClient.setAuthStatus(0x01);
+    }
+    else {
         sendReply(client_fd, ERR_PASSWDMISMATCH(_clients[client_fd].getNickname(), _clients[client_fd].getHostName()));
+    }
 }
